59aac1df2c7ba075745263aa83161397ba3b566a
CYann <wcy623209668@vip.qq.com>
2017-05-24 14:55:46 +0800

    实现离线下载的简单视图处理

---
+++ app/src/main/java/com/example/longinusl33t/blive/OnProgressBarListener.java
@@ -0,0 +1,13 @@
+package com.example.longinusl33t.blive;
+
+/**
+ * Created by wcy62 on 2017-05-24.
+ */
+
+public interface OnProgressBarListener {
+    /**
+     * 数字进度条进度回调接口
+     */
+
+    void onProgressChange(int current, int max);
+}
