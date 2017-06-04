de7a42de588c1913a45fe8b58080d60177c9728b
CYann <wcy623209668@vip.qq.com>
2017-06-04 16:09:42 +0800

    实现读取本机SD卡的内存读取和显示在processbar上 和部分会员积分的页面细节处理，接下去是对recycleview的使用将其拼接成一个界面

---
+++ app/src/main/java/com/example/longinusl33t/blive/CommonUtil.java
@@ -0,0 +1,89 @@
+package com.example.longinusl33t.blive;
+
+import android.os.Environment;
+import android.os.StatFs;
+
+import java.io.File;
+
+/**
+ * Created by wcy62 on 2017-06-04.
+ */
+
+public class CommonUtil {
+
+    /**
+     * 检查SD卡是否存在
+     */
+    private static boolean checkSdCard() {
+
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    }
+
+
+    /**
+     * 获取手机SD卡总空间
+     */
+    private static long getSDcardTotalSize() {
+
+        if (checkSdCard()) {
+            File path = Environment.getExternalStorageDirectory();
+            StatFs mStatFs = new StatFs(path.getPath());
+            long blockSizeLong = mStatFs.getBlockSizeLong();
+            long blockCountLong = mStatFs.getBlockCountLong();
+
+            return blockSizeLong * blockCountLong;
+        } else {
+            return 0;
+        }
+    }
+
+
+    /**
+     * 获取SDka可用空间
+     */
+    private static long getSDcardAvailableSize() {
+
+        if (checkSdCard()) {
+            File path = Environment.getExternalStorageDirectory();
+            StatFs mStatFs = new StatFs(path.getPath());
+            long blockSizeLong = mStatFs.getBlockSizeLong();
+            long availableBlocksLong = mStatFs.getAvailableBlocksLong();
+            return blockSizeLong * availableBlocksLong;
+        } else {
+            return 0;
+        }
+    }
+
+
+    /**
+     * 获取手机内部存储总空间
+     */
+    public static long getPhoneTotalSize() {
+
+        if (!checkSdCard()) {
+            File path = Environment.getDataDirectory();
+            StatFs mStatFs = new StatFs(path.getPath());
+            long blockSizeLong = mStatFs.getBlockSizeLong();
+            long blockCountLong = mStatFs.getBlockCountLong();
+            return blockSizeLong * blockCountLong;
+        } else {
+            return getSDcardTotalSize();
+        }
+    }
+
+
+    /**
+     * 获取手机内存存储可用空间
+     */
+    public static long getPhoneAvailableSize() {
+
+        if (!checkSdCard()) {
+            File path = Environment.getDataDirectory();
+            StatFs mStatFs = new StatFs(path.getPath());
+            long blockSizeLong = mStatFs.getBlockSizeLong();
+            long availableBlocksLong = mStatFs.getAvailableBlocksLong();
+            return blockSizeLong * availableBlocksLong;
+        } else
+            return getSDcardAvailableSize();
+    }
+}
--- app/src/main/java/com/example/longinusl33t/blive/DownloadActivity.java
+++ app/src/main/java/com/example/longinusl33t/blive/DownloadActivity.java
@@ -7,14 +7,17 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.format.Formatter;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class DownloadActivity extends AppCompatActivity {
+
 
     private TabLayout tabs;
     private ViewPager viewPager ;
@@ -39,6 +42,25 @@ public class DownloadActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //设置返回按钮显示
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        long phoneTotalSize = CommonUtil.getPhoneTotalSize();
+        long phoneAvailableSize = CommonUtil.getPhoneAvailableSize();
+        //转换为G的显示单位
+        String totalSizeStr = Formatter.formatFileSize(this, phoneTotalSize);
+        String availabSizeStr = Formatter.formatFileSize(this, phoneAvailableSize);
+        //计算占用空间的百分比
+        int progress = countProgress(phoneTotalSize, phoneAvailableSize);
+
+        NumberProgressBar mProgressBar = (NumberProgressBar) findViewById(R.id.progress_bar);
+        TextView mCacheSize =(TextView) findViewById(R.id.cache_size_text);
+
+        mProgressBar.setProgress(progress);
+        mCacheSize.setText("主存储:" + totalSizeStr + "/" + "可用:" + availabSizeStr);
+
+        CustomEmptyView mEmptyLayout = (CustomEmptyView) findViewById(R.id.empty_layout);
+        assert mEmptyLayout != null;
+        mEmptyLayout.setEmptyImage(R.drawable.img_tips_error_no_downloads);
+        mEmptyLayout.setEmptyText("没有找到你的缓存哟");
     }
 
     //调用toolbar布局
@@ -78,5 +100,15 @@ public class DownloadActivity extends AppCompatActivity {
     }
 
 */
+
+    private int countProgress(long phoneTotalSize, long phoneAvailableSize) {
+
+        double totalSize = phoneTotalSize / (1024 * 3);
+        double availabSize = phoneAvailableSize / (1024 * 3);
+        //取整相减
+        int size = (int) (Math.floor(totalSize) - Math.floor(availabSize));
+        double v = (size / Math.floor(totalSize)) * 100;
+        return (int) Math.floor(v);
+    }
 
 }
